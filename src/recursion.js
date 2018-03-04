227e655dd257a082bc728c2378ac8c2eb5f9d97f
seungprk@gmail.com <seungprk@gmail.com>
2018-03-03 22:42:45 -0800

    Implement negative portion of sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -57,11 +57,15 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-  if (n === 1 || n === 0) {
+  if (n === 1 || n === 0 || n === -1) {
     return 0;
   } 
 
-  return (n - 1) + sumBelow(n - 1);
+  if (n > 0) {
+    return (n - 1) + sumBelow(n - 1);
+  } else {
+    return (n + 1) + sumBelow(n + 1);
+  }
 };
 
 // 6. Get the integers within a range (x, y).
